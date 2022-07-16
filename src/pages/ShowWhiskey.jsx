435092b5c6d462607d3b7194b61a97497d32236c
Chris Back <chrisback011996@gmail.com>
2022-07-16 15:01:13 +1000

    refactored add comment form

--- src/pages/ShowWhiskey.jsx
+++ src/pages/ShowWhiskey.jsx
@@ -2,28 +2,31 @@ import React, { useContext } from "react";
 import { Link, useParams } from "react-router-dom";
 import AddComment from "../components/comments/AddComment";
 import { GlobalWhiskeyContext } from "../hooks/GlobalWhiskey";
-import { GlobalCommentContext } from "../hooks/globalComment"
+import { GlobalCommentContext } from "../hooks/globalComment";
 
 function ShowWhiskey() {
   const { whiskeys } = useContext(GlobalWhiskeyContext);
-      const { comments } = useContext(GlobalCommentContext);
+  const { comments } = useContext(GlobalCommentContext);
   const { id } = useParams();
 
   return (
     <>
-      <ul>
-        <li>Whiskey: {whiskeys[id].name}</li>
-        <li>Age: {whiskeys[id].age}</li>
-        <li>Type: {whiskeys[id].type}</li>
-      </ul>
+      {whiskeys
+        .filter((whiskey) => whiskey._id === id)
+        .map((singleWhiskey) => (
+          <>
+            <li>Name: {singleWhiskey.name}</li>
+            <li>Age: {singleWhiskey.age}</li>
+          </>
+        ))}
       <div>
         <AddComment />
-      </div>
-      <h3>Comments</h3>
+      </div>{" "}
+      {/* <h3>Comments</h3>
       <p>Comment: {comments[id].whiskey}</p>
       <p>Visual Rating: {comments[id].visual_rating}</p>
-      <button onClick={() => console.log(comments)}>Debug for comments</button>
-      </>
+      <button onClick={() => console.log(comments)}>Debug for comments</button> */}
+    </>
   );
 }
 
