d2355e0c7d524750d6bafbf5de62e86c9d15943d
Sonny949 <sonnypeterson949@hotmail.com>
2022-07-17 19:25:28 +1000

    changing branch

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,6 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
+import LoginButton from "../components/Login";
 
 const Profile = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
@@ -9,12 +10,17 @@ const Profile = () => {
   }
 
   return (
-    isAuthenticated && (
+    isAuthenticated ? (
       <div>
         <img src={user.picture} alt={user.name} />
         <h2>{user.name}</h2>
         <p>{user.email}</p>
       </div>
+    ) : (
+      <>
+        <h1>Oops, you're not logged in!</h1>
+        <LoginButton className="lg:btn" />
+      </>
     )
   );
 };
