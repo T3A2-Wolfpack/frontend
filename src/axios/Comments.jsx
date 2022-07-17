0d2c73cea70cc67c6dcd601c22fdb7f150f1d958
Sonny949 <sonnypeterson949@hotmail.com>
2022-07-17 13:01:56 +1000

    redeploy

--- src/axios/Comments.jsx
+++ src/axios/Comments.jsx
@@ -5,6 +5,7 @@ import { GlobalCommentContext } from "../hooks/globalComment";
 import { useParams } from "react-router-dom";
 
 // const { comments, showComments } = useContext(GlobalCommentContext)
+const api = import.meta.API_ENDPOINT || 'http://localhost:4000/api/whiskeys'
 
 export function GetComments(id) {
   const { showComments } = useContext(GlobalCommentContext)
@@ -13,7 +14,7 @@ export function GetComments(id) {
     useEffect(() => {
       async function retrieveComments() {
         res = await axios.get(
-          `http://localhost:4000/api/whiskeys/${id}/tastings`
+          `${api}/${id}/tastings`
         );
         await showComments;
       }
