7ae04e5384481aab92b808e26bd095cb7cc69a02
Chris Back <chrisback011996@gmail.com>
2022-07-18 16:13:52 +1000

    Saving

--- src/pages/NewWhiskey.jsx
+++ src/pages/NewWhiskey.jsx
@@ -3,7 +3,7 @@ import { Link, Navigate, useNavigate } from "react-router-dom";
 import { GlobalWhiskeyContext } from "../hooks/GlobalWhiskey";
 import { useContext, useReducer, useState } from "react";
 import formReducer from "../hooks/formReducer";
-// import { PostRequest } from "../axios/retrieveWhiskeyFromApi";
+import { PostRequest } from "../axios/retrieveWhiskeyFromApi";
 
 
 const api = import.meta.env.API_ENDPOINT || 'http://localhost:4000/api/whiskeys'
