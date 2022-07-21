8cb7be15709e775125b22c517789aebd955b4ac6
David <quachddavid@gmail.com>
2022-07-21 22:07:50 +1000

    added conditional rendering for logout/in

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,29 +1,28 @@
-import { Fragment, useContext, useState } from 'react'
-import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { MenuIcon, XIcon } from '@heroicons/react/outline'
-import { Link } from "react-router-dom"
-import logo from "../images/hwhiskey-logo.png"
-import LoginButton from './Login'
-import LogoutButton from './Logout'
-import Profile from '../pages/Profile'
-import { useAuth0 } from '@auth0/auth0-react'
-import SearchBar from './SearchBar'
-import { GlobalWhiskeyContext } from '../hooks/GlobalWhiskey'
-import LoginModal from './LoginModal'
+import { Fragment, useContext, useState } from "react";
+import { Disclosure, Menu, Transition } from "@headlessui/react";
+import { MenuIcon, XIcon } from "@heroicons/react/outline";
+import { Link } from "react-router-dom";
+import logo from "../images/hwhiskey-logo.png";
+import LoginButton from "./Login";
+import LogoutButton from "./Logout";
+import Profile from "../pages/Profile";
+import { useAuth0 } from "@auth0/auth0-react";
+import SearchBar from "./SearchBar";
+import { GlobalWhiskeyContext } from "../hooks/GlobalWhiskey";
+import LoginModal from "./LoginModal";
 import RegisterModal from "./RegisterModal";
 import { useLogout } from "../hooks/useLogout";
 import { useAuthContext } from "../hooks/useAuthContext";
 
-
-// create resources for Link to map over 
+// create resources for Link to map over
 const navigation = [
-  { name: 'Home', to: '/', current: true },
-  { name: 'Whiskies', to: '/whiskeys', current: false },
-  { name: 'Add a Whiskey', to: '/newwhiskey', current: false },  
-]
+  { name: "Home", to: "/", current: true },
+  { name: "Whiskies", to: "/whiskeys", current: false },
+  { name: "Add a Whiskey", to: "/newwhiskey", current: false },
+];
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(" ");
 }
 
 function Nav() {
@@ -33,7 +32,6 @@ function Nav() {
   const { logout } = useLogout();
   const { user } = useAuthContext();
 
-  
   return (
     <Disclosure as="nav" className="bg-amber-800">
       {({ open }) => (
@@ -88,25 +86,32 @@ function Nav() {
                   </div>
                 </div>
               </div>
-              
+
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                {user ? <>
-                  <p>welcome {user.name}</p>
-                  <button onClick={logout}>cl</button>
-                  
-                </> : ""}
-                <LoginModal
-                  logInModal={logInModal}
-                  setLogInModal={setLogInModal}
-                  setRegisterModal={setRegisterModal}
-                ></LoginModal>
-                <RegisterModal
-                  registerModal={registerModal}
-                  setRegisterModal={setRegisterModal}
-                ></RegisterModal>
-                
-                     
-                
+                {user && (
+                  <>
+                    <p>welcome {user.name}</p>
+                    <button
+                      onClick={logout}
+                      className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    >
+                      Log out
+                    </button>
+                  </>
+                )}
+                {!user && (
+                  <>
+                    <LoginModal
+                      logInModal={logInModal}
+                      setLogInModal={setLogInModal}
+                      setRegisterModal={setRegisterModal}
+                    ></LoginModal>
+                    <RegisterModal
+                      registerModal={registerModal}
+                      setRegisterModal={setRegisterModal}
+                    ></RegisterModal>
+                  </>
+                )}
               </div>
             </div>
           </div>
@@ -129,19 +134,18 @@ function Nav() {
                     >
                       {item.name}
                     </Link>
-                    
-                      <Link
-                        key="Make a Tasting"
-                        to="/tasting"
-                        className={classNames(
-                          item.current
-                            ? "bg-amber-900 text-white"
-                            : "text-gray-300 hover:bg-amber-700 hover:text-white",
-                          "px-3 py-2 rounded-md text-sm font-medium"
-                        )}
-                        aria-current={item.current ? "page" : undefined}
-                      />
-                   
+
+                    <Link
+                      key="Make a Tasting"
+                      to="/tasting"
+                      className={classNames(
+                        item.current
+                          ? "bg-amber-900 text-white"
+                          : "text-gray-300 hover:bg-amber-700 hover:text-white",
+                        "px-3 py-2 rounded-md text-sm font-medium"
+                      )}
+                      aria-current={item.current ? "page" : undefined}
+                    />
                   </Disclosure.Button>
                 </div>
               ))}
@@ -153,4 +157,4 @@ function Nav() {
   );
 }
 
-export default Nav
+export default Nav;
