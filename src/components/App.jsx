cb7699ab3bea946ea9c8691d08b4d8c28ee5efc5
Chris Back <chrisback011996@gmail.com>
2022-07-22 03:44:19 +1000

    DEBUGGED NAV

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,18 @@
+import { useContext, useState } from "react";
+import { BrowserRouter, Route, Routes, useParams } from "react-router-dom";
+// import "../App.css";
+import Nav from "./Nav";
+import Home from "../pages/Home";
+import NewWhiskey from "../pages/NewWhiskey";
+import Whiskeys from "../pages/Whiskeys";
+import ShowWhiskey from "../pages/ShowWhiskey";
+import EditAWhiskey from "../pages/EditAWhiskey";
+import Profile from "../pages/Profile";
 import { RetrieveWhiskeyFromApi } from "../axios/RetrieveWhiskeyFromApi";
-
+import { ProtectedRoute } from "./ProtectedRoute";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Loading } from "./Loading";
+import { AuthContextProvider } from "../context/AuthContext";
 
 import {
   GlobalWhiskeyContext,
@@ -9,8 +22,6 @@ import {
   GlobalCommentContext,
   GlobalCommentProvider,
 } from "../hooks/globalComment";
-import { AuthContextProvider } from "../context/AuthContext";
-import RouteComponent from "./RouteComponent";
 
 // define api from backend
 const api = "http://localhost:4000/api/whiskeys";
@@ -18,17 +29,45 @@ const api = "http://localhost:4000/api/whiskeys";
 function App() {
   // state where we are pushing the newly added whiskey
 
+  // const { isLoading } = useAuth0();
 
   // if (isLoading) {
   //   return <Loading type={"cylon"} color={"red"} />;
   // }
+
+  const { whiskeys } = useContext(GlobalWhiskeyContext);
+  const { comments } = useContext(GlobalCommentContext);
+
+  // // higher order component for showwhiskey
+  const ShowWhiskeyHOC = () => {
+    const { id } = useParams();
+    return <ShowWhiskey whiskey={whiskeys[id]} comment={comments[id]} />;
+  };
+
+  const EditWhiskeyHOC = () => {
+    const { id } = useParams();
+    return <EditAWhiskey whiskey={whiskeys[id]} />;
+  };
 
   return (
     <AuthContextProvider>
       <GlobalCommentProvider>
         <GlobalWhiskeyProvider>
           <RetrieveWhiskeyFromApi />
-          <RouteComponent />
+          <BrowserRouter>
+            <Nav />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/whiskeys" element={<Whiskeys />} />
+              <Route
+                path="/newwhiskey"
+                element={<ProtectedRoute component={NewWhiskey} />}
+              />
+              <Route path="/whiskey/:id" element={<ShowWhiskeyHOC />} />
+              <Route path="/whiskey/edit/:id" element={<EditWhiskeyHOC />} />
+              <Route path="/profile" element={<Profile />} />
+            </Routes>
+          </BrowserRouter>
         </GlobalWhiskeyProvider>
       </GlobalCommentProvider>
     </AuthContextProvider>
@@ -36,3 +75,11 @@ function App() {
 }
 
 export default App;
+
+
+
+
+
+
+
+
